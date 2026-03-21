d3f97293bddffd873fac2686fb240e9de1ba31d0
spriveen <rpeanuka@gmail.com>
2026-03-21 05:37:40 -0700

    update code

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from  'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const port = 4000;
 
 // MIDDLEAWARE
+app.use(cors());
+app.use(express.json({limit: "20mb"}));
+app.use(express.urlencoded({limit: "20mb", extended: true}));
 
 // DB
 
