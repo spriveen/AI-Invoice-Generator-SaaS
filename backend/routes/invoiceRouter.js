6bed0413ba5c1a0adb73c2250f7515222177042c
spriveen <rpeanuka@gmail.com>
2026-03-22 09:23:29 +0530

    updates Routes

---
+++ backend/routes/invoiceRouter.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+
+import { clerkMiddleware } from '@clerk/express';
+import { createInvoice, deleteInvoice, getInvoiceById, getInvoices, updateInvoice } from '../controllers/invoiceController.js';
+
+const invoiceRouter = express.Router();
+
+invoiceRouter.use(clerkMiddleware());
+
+invoiceRouter.get("/", getInvoices);
+invoiceRouter.get("/:id", getInvoiceById);
+invoiceRouter.post("/", createInvoice);
+invoiceRouter.put("/:id", updateInvoice);
+invoiceRouter.delete("/:id", deleteInvoice);
+
+export default invoiceRouter;
+
