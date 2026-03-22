6d85a61f2f803da2db434c0392eaaec0e06dae03
spriveen <rpeanuka@gmail.com>
2026-03-22 09:53:01 +0530

    update business profile.jsx

---
+++ backend/models/businessProfileModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { isLowercase } from "validator";
+
+const businessProfileSchema = new mongoose.Schema({
+    owner: {type: String, required: true, index: true},
+
+    businessName: {type: String, required:true},
+    email: {type: String, required: false, trim: true, lowercase: true,default:""},
+    address: {type: String, required: false, default: ""},
+    phone:{type: String, required: false, default: ""},
+    gst:{type: String, required: false, default: ""},
+
+
+    // for images
+    logoUrl:{type: String, required: false, default: "null"},
+    stampUrl:{type: String, required: false, default: "null"},
+    signatureUrl: {type: String, required: false, default:null},
+
+
+ signatuOwnerName: {type: String, required: false, default:""},
+ signatuOwnerTitle:{ required: false, default:""},
+
+defaultTaxPercent: {type: Number , required:false, default: 18},
+
+},{
+    timestamps:truue
+});
+
+const BusinessProfile = mongoose.models.BusinessProfile || mongoose.model("BusinessProfile", businessProfileSchema);
+
+export default BusinessProfile;
